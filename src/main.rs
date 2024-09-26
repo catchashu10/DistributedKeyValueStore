4374076ee7336e93c541e66c2e5f87f4070255f4
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2024-09-26 15:35:40 -0500

    Added thread safe LRU cache to the server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tonic::{transport::Server, Request, Response, Status};
 
+use moka::future::Cache;
+
 use datastore::data_store_server::{DataStore, DataStoreServer};
 use datastore::{GetRequest, GetResponse, PutRequest, PutResponse};
 
@@ -8,8 +10,11 @@ use sqlite::KeyValueDataStore;
 mod datastore;
 mod sqlite;
 
+const CACHE_SIZE: usize = 10_000;
+
 pub struct KeyValueServer {
     db: KeyValueDataStore,
+    cache: Cache<String, String>,
 }
 
 #[tonic::async_trait]
@@ -20,8 +25,17 @@ impl DataStore for KeyValueServer {
     ) -> Result<Response<GetResponse>, Status> {
         let key = request.into_inner().key;
 
+        // Check the cache if the key exists
+        if let Some(cached_value) = self.cache.get(&key.clone()).await {
+            return Ok(Response::new(GetResponse { value: cached_value, found: true }));
+        }
+
         match self.db.get(&key) {
-            Ok(Some(value)) => Ok(Response::new(GetResponse { value, found: true })),
+            Ok(Some(value)) => {
+                // Cache the value
+                self.cache.insert(key.clone(), value.clone()).await;
+                Ok(Response::new(GetResponse { value, found: true }))
+            },
             Ok(None) => Ok(Response::new(GetResponse { value: "".to_string(), found: false })),
             Err(e) => Err(Status::internal(format!("DB error: {}", e))),
         }
@@ -34,7 +48,11 @@ impl DataStore for KeyValueServer {
         let PutRequest { key, value } = request.into_inner();
 
         match self.db.put(&key, &value) {
-            Ok(old_value) => Ok(Response::new(PutResponse { value: old_value })),
+            Ok(old_value) => {
+                // Cache the value
+                self.cache.insert(key.clone(), value.clone()).await;
+                Ok(Response::new(PutResponse { value: old_value }))
+            },
             Err(e) => Err(Status::internal(format!("DB error: {}", e))),
         }
     }
@@ -44,7 +62,12 @@ impl DataStore for KeyValueServer {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse().unwrap();
     let db = KeyValueDataStore::new("kv_store.db").expect("Failed to create datastore");
-    let store = KeyValueServer { db };
+
+    let cache = Cache::builder()
+        .max_capacity(CACHE_SIZE as u64)
+        .build();
+
+    let store = KeyValueServer { db, cache };
     let server = DataStoreServer::new(store);
     println!("Starting gRPC server on {}", addr);
 
